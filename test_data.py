83c502f62afe1d73a1c0eb80d50e9e76151a1df9
Andrei Maksimov <a.maksimov@brandquad.ru>
2024-04-30 15:32:25 +0400

    register all tests

--- test_data.py
+++ test_data.py
@@ -1,2 +1,6 @@
 
 curl = "https://stellarburgers.nomoreparties.site"
+
+email = "test_user@yandex.ru"
+password = "12345"
+name = "User"
--- tests/register/test_register.py
+++ tests/register/test_register.py
@@ -1,17 +1,46 @@
 import pytest
 import json
+
+import test_data
 from helpers import Helpers
 
 class TestRegister:
 
     def test_register_new_user(self):
         helpers = Helpers()
-
         payload = helpers.generate_random_data_payload()
 
         responce = helpers.registration_user(payload)
 
         assert responce.status_code == 200 and responce.json()["user"]["email"] == payload["email"]
+
+
+
+    def test_duplicate_user(self):
+        helpers = Helpers()
+        payload = helpers.generate_random_data_payload()
+        helpers.registration_user(payload)
+
+        responce = helpers.registration_user(payload)
+
+        assert responce.status_code == 403 and responce.json()["message"] == "User already exists"
+
+    @pytest.mark.parametrize("payload", [
+        {"password": test_data.password, "name": test_data.name},
+        {"email": test_data.email, "name": test_data.name},
+        {"email": test_data.email, "password": 'test_data.password'}
+
+    ])
+    def test_not_all_fields(self, payload):
+        helpers = Helpers()
+
+
+        responce = helpers.registration_user(payload)
+
+        assert responce.status_code == 403 and responce.json()["message"] == "Email, password and name are required fields"
+
+
+
 
 
 
